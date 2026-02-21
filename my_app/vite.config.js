5edc5a8e93ebd14f4c0dd90329468d58fb011a8e
lebinhantd-glitch <lebinhandtd@gmail.com>
2026-02-21 17:29:24 +0700

    update

--- my_app/vite.config.js
+++ my_app/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
+import path from "path"
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resole: {
+    alias:{
+      "@": path.resolve(__dirname,"./src"),
+    },
+  },
+});
